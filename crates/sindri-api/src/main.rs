eff4dc5940763e62e2a3e9cabbd395d01dca3328
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2025-09-30 20:08:58 -0600

    feat(api): added simple axum app and get endpoint

--- crates/sindri-api/src/main.rs
+++ crates/sindri-api/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = Router::new()
+        .route("/", get(async || "OK"))
+        .route("/hello-world", get(hello_world));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+    let listener = TcpListener::bind(addr).await?;
+    let _ = axum::serve(listener, app).await?;
+    Ok(())
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
 }
