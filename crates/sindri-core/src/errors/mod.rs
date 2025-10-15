3896ec033621ab980b84fcf2eaccdaf74c0e2eff
Miguel <Elizalde>
2025-10-15 15:54:57 -0600

    feat(errors): add SocketError enum for socket-related errors

--- crates/sindri-core/src/errors/mod.rs
+++ crates/sindri-core/src/errors/mod.rs
@@ -1,8 +1,10 @@
+pub mod socket;
 pub mod vm;
 
 #[derive(Debug)]
 pub enum SindriError {
     Vm(vm::VMError),
+    Socket(socket::SocketError),
 }
 
 pub type Result<T> = std::result::Result<T, SindriError>;
